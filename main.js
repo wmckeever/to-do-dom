ae57ba18f0698260bca528a0309f02ad56558700
wmckeever <wil.mckeever@gmail.com>
2022-11-09 14:05:23 -0500

    Finaled as per requirements

--- main.js
+++ main.js
@@ -3,9 +3,8 @@ let removeAll = document.querySelector('#removeAllButton');
 let removeCompleted = document.querySelector('#removeCompletedButton');
 let addButton = document.querySelector('#addButton');
 let orderedList = document.querySelector('ol');
-let items = document.querySelector('li');
 let input = document.querySelector('#listItem');
-
+let totals = document.querySelector('#tasksRemaining');
 
 
 
@@ -17,37 +16,57 @@ let input = document.querySelector('#listItem');
 addButton.addEventListener('click', function(event){
   event.preventDefault();
   console.log("Add Item Button Works")
-  
+
+  //empty input detection
+if(input.value === ""){
+  alert("Please enter a valid item to add it to the list. ")
+  document.remove("li")
+}
+
   let listItem = document.createElement("li");
-
-//*<------------- Mark Items As Complete ------------->*//
-listItem.addEventListener('click', function(){
-
-  if(listItem.style.textDecoration === "line-through"){
-    listItem.style.textDecoration = "";
-    listItem.innerText = input.value;
-      }else if(listItem.style.textDecoration === ""){
-    listItem.style.textDecoration = "line-through"
-    listItem.innerText = input.value + " - completed";
-   
-
-  }
+  listItem.classList.add('allItems');
+  
+  
+  
+  //*<------------- Mark Items As Complete ------------->*//
+  listItem.addEventListener('click', function(){
+    
+    //listItem.innerText = input.value;
+    
+    if(listItem.style.textDecoration === "line-through"){
+      listItem.style.textDecoration = "";
+      console.log(listItem.classList);
+    }else if(listItem.style.textDecoration === ""){
+      listItem.style.textDecoration = "line-through"
+      //listItem.innerText = input.value += " - completed";
+      listItem.classList.add('completed');
+      console.log(listItem.classList);
+    }
   });
-
+  
   listItem.innerText = input.value;
   orderedList.appendChild(listItem);
+  input.value = "";
   
+});
+//*<------------- Remove Completed Items ------------->*//
+removeCompleted.addEventListener('click', function(event){
+  event.preventDefault();
+  let completed = document.querySelectorAll('.completed');
+
+  completed.forEach(item => {item.remove()});
 });
 
 
 //*<------------- Remove All Items ------------->*//
-
-removeAll.addEventListener('click', function(event){
-  event.preventDefault();
-  let removeItems = document.querySelectorAll('ol li');
-    for (let i = 0; i < removeItems.length; i++) {
-        olItem1.removeChild(removeItems[i]);
-    }
-  
+removeAll.addEventListener('click', (e)=>{
+  e.preventDefault();
+  let allItems = document.querySelectorAll('ol li');
+  allItems.forEach(item => {item.remove()});
 });
 
+//*<------------- Display Total ------------->*//
+for (let i = 0; i < orderedList.length; i++) {
+  console.log(orderedList.length);
+  
+}
