302908a70d15bb657c3e52aea419c04a001df21f
wmckeever <wil.mckeever@gmail.com>
2022-11-10 11:27:10 -0500

    Finalized WORKING CODE

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ let orderedList = document.querySelector('ol');
 let input = document.querySelector('#listItem');
 let totals = document.querySelector('#tasksRemaining');
 
+let itemCount = document.querySelectorAll('ol li');
+let totalCount = document.querySelector('#totalCount');
 
 
 //*<------------- Functional Code ------------->*//
@@ -17,60 +19,68 @@ addButton.addEventListener('click', function(event){
   event.preventDefault();
   console.log("Add Item Button Works")
 
-  //empty input detection
-if(input.value === ""){
-  alert("Please enter a valid item to add it to the list. ")
-  document.remove("li")
-}
-
-  let listItem = document.createElement("li");
-  listItem.classList.add('allItems');
-  
-  
-  
-  //*<------------- Mark Items As Complete ------------->*//
-  listItem.addEventListener('click', function(){
-    
-    //listItem.innerText = input.value;
-    
-    if(listItem.style.textDecoration === "line-through"){
-      listItem.style.textDecoration = "";
-      console.log(listItem.classList);
-    }else if(listItem.style.textDecoration === ""){
-      listItem.style.textDecoration = "line-through"
-      //listItem.innerText = input.value += " - completed";
-      listItem.classList.add('completed');
-      console.log(listItem.classList);
+      //empty input detection
+    if(input.value === ""){
+      alert("Please enter a valid item to add it to the list. ")
+      listItem.remove("li")
     }
+
+      let listItem = document.createElement("li");
+      listItem.classList.add('allItems');
+
+      
+      
+      //*<------------- Mark Items As Complete ------------->*//
+      listItem.addEventListener('click', function(){
+      
+        
+        if(listItem.style.textDecoration === "line-through"){
+          listItem.style.textDecoration = "";
+          console.log(listItem.classList);
+        }else if(listItem.style.textDecoration === ""){
+          listItem.style.textDecoration = "line-through"
+          
+          listItem.classList.add('completed');
+          console.log(listItem.classList);
+        }
+      });
+      
+      listItem.innerText = input.value;
+      orderedList.appendChild(listItem);
+      input.value = "";
+    
+      
+      itemCount = document.querySelectorAll('ol li');
+      totalCount.innerText = itemCount.length;
+    });
+
+  //*<------------- Remove Completed Items ------------->*//
+  removeCompleted.addEventListener('click', function(event){
+    event.preventDefault();
+    let completed = document.querySelectorAll('.completed');
+    completed.forEach(item => {item.remove()});
+
+
+    itemCount = document.querySelectorAll('ol li');
+      totalCount.innerText = itemCount.length;
   });
+
+
+        //*<------------- Remove All Items ------------->*//
+        removeAll.addEventListener('click', (e)=>{
+          e.preventDefault();
+          let allItems = document.querySelectorAll('ol li');
+          allItems.forEach(item => {item.remove()});
+          
+
+              
+        itemCount = document.querySelectorAll('ol li');
+        totalCount.innerText = itemCount.length;
+            });
+
+        
   
-  listItem.innerText = input.value;
-  orderedList.appendChild(listItem);
-  input.value = "";
-  
-});
-//*<------------- Remove Completed Items ------------->*//
-removeCompleted.addEventListener('click', function(event){
-  event.preventDefault();
-  let completed = document.querySelectorAll('.completed');
 
-  completed.forEach(item => {item.remove()});
-});
-
-
-//*<------------- Remove All Items ------------->*//
-removeAll.addEventListener('click', (e)=>{
-  e.preventDefault();
-  let allItems = document.querySelectorAll('ol li');
-  allItems.forEach(item => {item.remove()});
-});
-
-//*<------------- Display Total ------------->*//
-let itemCount = querySelectorAll('ol li');
-let totalCount = querySelector('#totalCount');
-
-for (let i = 0; i < itemCount.length; i++) {
-
- totalCount.innerText = itemCount.length;
-  
-}
+        
+          console.log(totalCount);
+       
